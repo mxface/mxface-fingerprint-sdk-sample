f6d7715e7372caeb2115326fbd4012b6b51968c7
Aayush Thakkar <aayush.thakkar@mantratec.com>
2025-06-18 19:04:35 +0530

    Update SQL query

--- Artifacts/SQL/script.sql
+++ Artifacts/SQL/script.sql
@@ -1,15 +1,12 @@
-CREATE TABLE IF NOT EXISTS "__FingerprintSubjects"
+CREATE TABLE IF NOT EXISTS public."__FingerprintSubjects"
 (
-    "Id" TEXT NOT NULL,
-    "SubjectId" TEXT,
-    "TemplateTypeId" TEXT,
-    "Template" BLOB,
-    "CreatedDate" TEXT NOT NULL,
-    "ModifiedDate" TEXT,
-    "IsActive" INTEGER NOT NULL,
-    PRIMARY KEY ("Id"),
-    FOREIGN KEY ("TemplateTypeId") REFERENCES "__TemplateTypes" ("Id")
-);
+    "Id" integer NOT NULL DEFAULT nextval('"__FingerprintSubjects_Id_seq"'::regclass),
+    "SubjectId" character varying(45) COLLATE pg_catalog."default",
+    "Template" bytea,
+    "Group" character varying(45) COLLATE pg_catalog."default",
+    "ClientId" integer,
+    CONSTRAINT "__FingerprintSubjects_pkey" PRIMARY KEY ("Id")
+)
 
 CREATE TABLE IF NOT EXISTS "__TemplateTypes"
 (
